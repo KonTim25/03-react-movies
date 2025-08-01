80d8b28be06e7762b77edf4e59f49a1cd26d1b08
KonTim25 <ktimch@gmail.com>
2025-08-01 19:58:02 +0300

    edit api_url in movieService

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { type Movie } from '../types/movie.ts';
 
-const API_URL = 'https://api.themoviedb.org/3/search/movi';
+const API_URL = 'https://api.themoviedb.org/3/search/movie';
 
 export const fetchMovies = async (query: string): Promise<Movie[]> => {
     const response = await axios.get(API_URL, {
