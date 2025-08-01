641154b7c81ec91ce70a22f4446aa5c08e002d64
KonTim25 <ktimch@gmail.com>
2025-08-01 19:29:36 +0300

    result

---
+++ src/services/movieService.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { type Movie } from '../types/movie.ts';
+
+const API_URL = 'https://api.themoviedb.org/3/search/movi';
+
+export const fetchMovies = async (query: string): Promise<Movie[]> => {
+    const response = await axios.get(API_URL, {
+        params: {
+            query,
+            api_key: import.meta.env.VITE_TMDB_TOKEN,
+        },
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
+        },
+    });
+    return response.data.results;
+};
